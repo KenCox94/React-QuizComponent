5eb734925b734f104e4d1f79343c01dd3f37c930
Kenneth Cox <kenneth.cox@bluehalo.com>
2022-08-26 22:04:25 -0600

    updte

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,12 +12,11 @@ class Quiz extends Component {
     }
 
     render() {
-        const isQuizEnd = this.state.this.state.quiz_position - 1 === quizData.quiz_questions.length;
+        const isQuizEnd = this.state.this.state.quiz_position - 1 === quizData.quiz_questions.length
         return (
-            <div>
-                <QuizEnd />
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-            </div>
+            < div >
+                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+            </div >
         );
     }
 
